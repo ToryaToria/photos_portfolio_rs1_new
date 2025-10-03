05128411b7a8b4ec9c792e6d2da6688f01ba5122
ToryaToria <toria.torya@yandex.ru>
2025-10-03 09:32:29 +0300

    chore: add js

---
+++ source/scripts/main.js
@@ -0,0 +1,22 @@
+console.log('hi!');
+// let a +=10;
+
+//  let i = 0; 
+//  let c = 0; 
+//  const str = 'Rolling Scopes School'; 
+//  while (i < str.length) { if (str[i] === 'o') { c++; } i++; } console.log(c);
+
+//  const RS = 'rs'; console.log('css', RS, 'js');
+
+  // for (let i = 0; i <= 4; i++) {console.log(++i);}
+
+  // console.log(Math.abs(Math.floor(-46.211)));
+
+  // const nae = 'Vasia'; 
+  // console.log(Math.abs(nae.length - 15));
+//  console.log(typeof (1 + "2"));
+
+// const sum = (a, b) => a + b
+
+// let v = (!!'0');
+ console.log(typeof Symbol('JS'));
